72425c0aeb370753ea600fa817bd6e61a32dd519
SohagMollik <74552043+SohagMollik@users.noreply.github.com>
2021-02-26 02:27:13 +0600

    Add files via upload

--- 10699 Count the factors.cpp
+++ 10699 Count the factors.cpp
@@ -1,27 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define mal 100000
-bool bal[mal];
-int sieve(int n)
+#define max 1000000
+bool prime[max];
+int factor[max];
+int z=0;
+void sieve()
 {
-    int i,j,c=0;
-    for(i=1;i<=n;i++)bal[i]=true;
-    for(i=2;i*i<=n;i++){
-        if(bal[i]==true){
-            for(j=i;j<=n;j+=i)
-                c++;
+
+    int i,j;
+    for(i=1;i<=max;i++)prime[i]=true;
+    for(i=4;i<=max;i+=2)prime[i]=false;
+    for(i=3;i*i<=max;i++){
+        if(prime[i]==true){
+            for(j=i*i;j<=max;j+=i){
+               prime[j]=false;
+            }
+
         }
     }
-   // cout<<n<<" : "<<c<<endl;
-   return c;
+
+    factor[z]=2;
+     z=1;
+    for(i=3;i<=max;i++){
+        if(prime[i]==true)
+            factor[z++]=i;
+    }
+
 }
+
 int main()
 {
     int n;
-    while(cin>>n){
-        if(n==0)break;
-        else{
-            sieve(n);
+    sieve();
+    while(cin>>n&&n!=0){
+        int ok=0;
+        for(int k=0;k<z;k++){
+        if(n%factor[k]==0)
+            ok++;
+
         }
+        cout<<n<<" : "<<ok<<endl;
+
+
+
     }
 }
